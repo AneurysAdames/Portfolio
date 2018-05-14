aa96d9cb997c709b27333879397adaa90e3a90cb
Andrew Locke <alocke12992@gmail.com>
2018-05-14 12:25:06 -0600

    block out home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {Grid, Image, Segment} from 'semantic-ui-react';
+import Google from '../assets/photos/google.png';
+import Voice from '../assets/photos/Voice.png';
+import Typer from './Typer';
+import styled from 'styled-components';
 
-const Home = () => <div>Home</div>
+const Home = () => (
+  <Grid centered>
+    <Grid.Column width={5}>
+      <Grid.Row>
+        <Image src={Google} />
+      </Grid.Row>
+      <Grid.Row>
+        <SearchBar>
+          <Typer />
+          <Mic src={Voice} />
+        </SearchBar>
+      </Grid.Row>
+    </Grid.Column>
+  </Grid>
+)
+
+const SearchBar = styled(Segment) `
+  display: flex !important;
+  flex-direction: row !important;
+  justify-content: space-between !important;
+  align-items: center !important;
+`
+const Mic = styled.img`
+  height: 35px;
+
+`
 export default Home
---
+++ src/components/Typer.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+
+class Typer extends Component {
+  state = {examples: ["One", "Two", "Three"], description: 0}
+
+  componentDidMount() {
+    const subheader = document.getElementById('example')
+    let text = 'Lets connect...';
+    this.type(text, subheader);
+  }
+
+  deleteLine = (e, string) => {
+    const deleter = (i) => {
+      i--
+      if (i < 0) {
+        const {description, examples} = this.state
+        e.value = '';
+        this.setState({description: this.state.description + 1})
+        if (description === 2) {
+          this.setState({description: 0})
+        }
+        let currentExample = examples[description]
+        this.type(currentExample, e)
+        return;
+      }
+      e.innerText = string.substring(0, i);
+      setTimeout(() => {
+        deleter(i);
+      }, 50);
+    };
+    deleter(string.length + 1);
+  };
+
+  type = (string, e) => {
+    const writer = (i) => {
+      if (string.length <= i++) {
+        e.value = string;
+        setTimeout(() => {
+          this.deleteLine(e, string);
+        }, 1000);
+        return;
+      }
+      e.innerText = string.substring(0, i);
+      setTimeout(() => {
+        writer(i);
+      }, 50);
+    };
+    writer(0);
+  };
+
+  render() {
+    return (
+      <div id='example'></div>
+    );
+  }
+}
+
+
+
+export default Typer;
